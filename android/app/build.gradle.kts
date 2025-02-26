43d2883343c6230844d96ab14cf8f7de9453f0fb
Cj <keleher674@gmail.com>
2025-02-26 13:07:09 -0500

    Changing the name of the app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.SHI Devices"
+    namespace = "com.example.color_toggle_app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.SHI Devices"
+        applicationId = "com.example.color_toggle_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
